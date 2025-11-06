776c4ec0a25e0424cb4c78a77b56186a612d3737
手瓜一十雪 <nanaeonn@outlook.com>
2025-11-06 23:28:13 +0800

    Update load.cjs

--- load.cjs
+++ load.cjs
@@ -5,9 +5,8 @@ process.dlopen({ exports: {} }, './node_addon.node', os.constants.dlopen.RTLD_NO
 const BASE_DIR = __dirname;
 const WRAPPER_NODE_PATH = path.join(BASE_DIR, 'wrapper.node');
 const PACKAGE_JSON_PATH = path.join(BASE_DIR, 'package.json');
-const CONFIG_JSON_PATH = path.join(BASE_DIR, 'config.json');
 const NAPCAT_MJS_PATH = path.join(BASE_DIR, 'napcat', 'napcat.mjs');
 process.env.NAPCAT_WRAPPER_PATH = WRAPPER_NODE_PATH;
 process.env.NAPCAT_QQ_PACKAGE_INFO_PATH = PACKAGE_JSON_PATH;
-process.env.NAPCAT_QQ_VERSION_CONFIG_PATH = CONFIG_JSON_PATH;
+// process.env.NAPCAT_QQ_VERSION_CONFIG_PATH = CONFIG_JSON_PATH;
 import(pathToFileURL(NAPCAT_MJS_PATH).href);
